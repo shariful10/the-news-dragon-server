873965b33c5633b74cce4795c09ce849279525cc
Sk Shariful Islam <skshariful10@gmail.com>
2023-04-29 23:26:08 +0600

    Vercel Config File Added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 
@@ -13,6 +14,27 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
 	res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+	res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+	const id = req.params.id;
+	console.log(id);
+	const selectedNews = news.find((n) => n._id === id);
+	res.send(selectedNews);
+});
+
+app.get("/category/:id", (req, res) => {
+	const id = parseInt(req.params.id);
+	if (id === 0) {
+		res.send(news);
+	} else {
+		const categoryNews = news.filter((n) => parseInt(n.category_id) === id);
+		res.send(categoryNews);
+	}
 });
 
 app.listen(port, () => {
